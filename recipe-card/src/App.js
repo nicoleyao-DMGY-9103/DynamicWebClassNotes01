a95d9f9af6d50ca04943c3a089cc5b7dd4ac7013
Xingyu Yao <xy2779@nyu.edu>
2025-09-12 19:07:41 -0400

    2

--- recipe-card/src/App.js
+++ recipe-card/src/App.js
@@ -1,32 +1,7 @@
 import React from 'react'
-import RecipeCard from '../RecipeCard/index'
-
-const recipes = [
-  {
-    id: 1,
-    title: 'Buttermilk Pancakes',
-    description: 'Fluffy pancakes with butter and maple syrup.'
-  },
-  {
-    id: 2,
-    title: 'Avocado Toast',
-    description: 'Toasted sourdough topped with smashed avocado.'
-  },
-  {
-    id: 3,
-    title: 'Berry Smoothie',
-    description: 'Mixed berries blended with yogurt.'
-  }
-]
 
 function App() {
-  return (
-    <div>
-      {recipes.map(({ id, title, description }) => (
-        <RecipeCard key={id} title={title} description={description} />
-      ))}
-    </div>
-  )
+  return <div>Hello App!</div>
 }
 
 export default App
