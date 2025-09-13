0caef70e20289e8fc08d407af9cac0eaf839f305
Xingyu Yao <xy2779@nyu.edu>
2025-09-13 14:38:11 -0400

    Update App.js

--- recipe-card/src/App.js
+++ recipe-card/src/App.js
@@ -4,14 +4,34 @@ import './RecipeCard/components/RecipeCard.css'
 import RECIPE_IMG from './assets/pancake.png'
 import {RECIPE} from './RecipeCard/components/recipe-data'
 
-function App() {
-   return <div>Hello App!</div>
- }
+//function App() {
+   //return <div>Hello App!</div>
+ //}
 
-//const App = () => {
+const App = () => {
+    return (
+        <div className="card">
+          <img src={RECIPE_IMG} alt="yummy pancake" />
+    
+          <h2>{RECIPE.title}</h2>
+          <p>{RECIPE.description}</p>
+          <h3>Ingredients</h3>
+          <ul>
+            {RECIPE.ingredients.map((item, index) => {
+              return <li key={index}>{item}</li>
+            })}
+          </ul>
+    
+          <h3>Instructions</h3>
+          <ol>
+            {RECIPE.instructions.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ol>
+        </div>
+      )
 
-
-//}
+}
 
 
 
