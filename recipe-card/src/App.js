de3a5b2f7b50e7f6c09c48a3754b61c082fc319f
Xingyu Yao <xy2779@nyu.edu>
2025-09-13 14:28:42 -0400

    3

--- recipe-card/src/App.js
+++ recipe-card/src/App.js
@@ -1,17 +1,17 @@
-// import React from 'react'
+import React from 'react'
 
 import './RecipeCard/components/RecipeCard.css'
 import RECIPE_IMG from './assets/pancake.png'
 import {RECIPE} from './RecipeCard/components/recipe-data'
 
-// function App() {
-//   return <div>Hello App!</div>
-// }
+function App() {
+   return <div>Hello App!</div>
+ }
 
-const App = () => {
+//const App = () => {
 
 
-}
+//}
 
 
 
